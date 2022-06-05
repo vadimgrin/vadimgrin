879703d585e05c2cd1ad9dc69e81c1275e3f9264
Vadim Grinberg <vadim91@yahoo.com>
2022-06-05 16:52:10 -0400

    more changes

--- guessword.py
+++ guessword.py
@@ -3,12 +3,11 @@ import enchant
 
 if __name__ == '__main__':
     my_dict = enchant.Dict("en_US")
-    letters = 'qwtyupdfgajklzxcvbnm'
+    letters = 'qwydfgsjklzxcvnm'
     for first in letters:
         for second in letters:
-            for third in letters:
-                # ======= 1 == 2 === 3 === 4 === 5 ==
-                word = (' n    a    %s    %s     %s   ' % (first, second, third)).replace(' ', '')
-                if my_dict.check(word):
-                    print(word, my_dict.check(word))
+            # ======= 1 == 2 === 3 === 4 === 5 ==
+            word = (' s    l    u    %s     %s   ' % (first, second)).replace(' ', '')
+            if my_dict.check(word):
+                print(word, my_dict.check(word))
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import openpyxl
 from tkinter import *
+import matplotlib.pyplot as plt
 import logging
 
 logger = logging.getLogger(__name__)
@@ -34,19 +35,22 @@ class Form:
         try:
             self.data = data
             self.root = Tk()
+            self.root.title('Blood Tests')
             self.form = None
         except RuntimeError as e:
             logger.exception('Failed to open the file', e)
 
     def show(self):
-        self.form = Canvas(self.root, width=200, height=60)
-        self.form.pack()
 
         labels = self.data.labels.values
         metric = StringVar(self.root)
         w = OptionMenu(self.root, metric, labels[0], *labels)
-        w.pack()
-        mainloop()
+        self.root.geometry('600x400+80+80')
+        rightframe = Frame(self.root)
+        toplabel = Label(rightframe)
+        w.grid(column=0, row=0)
+        rightframe.grid(column=1, row=0)
+        self.root.mainloop()
 
 
 if __name__ == '__main__':
@@ -54,8 +58,11 @@ if __name__ == '__main__':
     _loc = "\\\MYBOOKLIVE\\Public\\Vadim Documents\\BloodTests Vadim\\Blood Test Results.xlsx"
     _data = Data(openpyxl.load_workbook(_loc), 'Vadim')
     _form = Form(_data)
+    print('Measures: ')
     print(_data.measures.describe())
+    print('Bounds: ')
     print(_data.bounds.describe())
+    print('Labels: ')
     print(_data.labels.describe())
 
     _form.show()
