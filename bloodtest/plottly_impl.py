4c80b0912043fbeab13daec41bc3b66c11b8c0e2
Vadim Grinberg <vadim91@yahoo.com>
2025-02-15 23:10:32 -0500

    Fully functional (needs color)

--- bloodtest/plottly_impl.py
+++ bloodtest/plottly_impl.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, Input, Output
+from dash import Dash, html, dcc, Input, Output, dash_table
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
@@ -16,31 +16,63 @@ class Data:
         row = self.data[self.data['Measure'] == label]
         row = row.fillna(method='ffill', axis=1)
         if not row.empty:
-            _lo = row.loc[:, 'Low Boundary'].values[-1]
-            _hi = row.loc[:, 'High Boundary'].values[-1]
-            row.drop(columns=['Measure', 'High Boundary', 'Low Boundary'], inplace=True)
             a = row.transpose()
             a = a.rename(columns=lambda x: 'Value')
+            a['Measure'] = a['Value']['Measure']
+            a['High Bound'] = a['Value']['High Boundary']
+            a['Low Bound'] = a['Value']['Low Boundary']
+            a.drop(['Measure', 'High Boundary', 'Low Boundary'], inplace=True)
+            a = a.astype({'Measure': 'string'})
             a['Value'] = pd.to_numeric(a['Value'])
-            a['High Bound'] = _hi
-            a['Low Bound'] = _lo
-        return a
+            a.index.astype('datetime64')
+        return a[['Measure', 'Value', 'Low Bound', 'High Bound']]
+
+    def dash_table(self, df):
+        mytable = dash_table.DataTable(
+            id='id_table1',
+            columns=[{"name": i, "id": i, 'type': 'numeric', 'format': {'specifier': ',.0f'}} if i not in [
+                'phone_number'] else {"name": i, "id": i} for i in df.columns],
+            data=df.to_dict('records'),
+            style_as_list_view=True,
+            style_cell={'padding': '2px'},
+            page_action='none',
+            style_table={'height': '600px', 'overflowY': 'auto'},
+            style_cell_conditional=[
+                {
+                    'if': {'column_id': 'phone_number'},
+                    'textAlign': 'center'
+                },
+            ],
+            style_data_conditional=[
+                {
+                    'if': {'row_index': 0},
+                    'backgroundColor': '#FFF2CC',
+                },
+            ],
+            style_header={
+                'backgroundColor': 'white',
+                'fontWeight': 'bold',
+                'textAlign': 'center'
+            },
+        )
+        return mytable
 
 
 _file_path = "\\\MYBOOKLIVE\\Public\\Vadim Documents\\BloodTests Vadim\\Blood Test Results.xlsx"
 _data = Data(_file_path, 'Vadim')
 app = Dash(__name__)
 
-
 app.layout = html.Div([
-    html.H1('Blood Test Results'),   # <== Row1 of Div1
-    html.Div([html.Div([html.Div([dcc.RadioItems(_data.labels, _data.labels[0], id='id_metric', labelStyle={'display': 'block'})], style={ 'display': 'inline-block', 'height': '810px', 'overflow-y': 'scroll'}),
+    html.H1('Blood Test Results'),  # <== Row1 of Div1
+    html.Div([html.Div([html.Div(
+        [dcc.RadioItems(_data.labels, _data.labels[0], id='id_metric', labelStyle={'display': 'block'})],
+        style={'display': 'inline-block', 'height': '810px', 'overflow-y': 'scroll'}),
                         html.Div([html.H1(id='id_title'),
                                   dcc.Graph(id='id_graph'),
-                                  html.Table([html.Thead(html.Tr([html.Th('A'),html.Th('B'),html.Th('C')])),html.Tbody([html.Tr([html.Td(1),html.Td(2),html.Td(3)])])])
-                                 ], style={'display': 'inline-block','vertical-align': 'top','width': '50vw'}),
-                ],style={'display': 'flex', 'flexDirection': 'row'})
-], style={'display': 'flex', 'flexDirection': 'column'})])
+                                  _data.dash_table(_data.get_data_toplot(_data.labels[0])),
+                                  ], style={'display': 'inline-block', 'vertical-align': 'top', 'width': '50vw'}),
+                        ], style={'display': 'flex', 'flexDirection': 'row'})
+              ], style={'display': 'flex', 'flexDirection': 'column'})])
 
 
 def getFigure(df, mode='px'):
@@ -49,7 +81,8 @@ def getFigure(df, mode='px'):
     else:
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=df.index, y=df['High Bound'], mode='lines', name=f'High ({df["High Bound"][0]})'))
-        fig.add_trace(go.Scatter(x=df.index, y=df['Value'], mode='lines+markers+text', name='Value', text=df['Value'], textposition='top center'))
+        fig.add_trace(go.Scatter(x=df.index, y=df['Value'], mode='lines+markers+text', name='Value', text=df['Value'],
+                                 textposition='top center'))
         fig.add_trace(go.Scatter(x=df.index, y=df['Low Bound'], mode='lines', name=f'Low ({df["Low Bound"][0]})'))
         return fig
 
